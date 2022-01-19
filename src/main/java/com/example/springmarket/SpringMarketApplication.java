4f2d47133a392b0978b389cd3bff399e5a3b386c
goodillich2 <goodillich3@gmail.com>
2022-01-19 15:56:53 +0200

    added entitys, sevice classes(), repositories

--- src/main/java/com/example/springmarket/SpringMarketApplication.java
+++ src/main/java/com/example/springmarket/SpringMarketApplication.java
@@ -8,7 +8,7 @@ public class SpringMarketApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMarketApplication.class, args);
-        System.out.println("kek");
+        System.out.println("kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111kek111111111111");
     }
 
 }
